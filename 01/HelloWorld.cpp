f063ceadeaf3811a82e2b61ffe501eb096fc7953
cp-edo <edoardo.olivero@edu.itspiemonte.it>
2024-09-16 13:47:32 +0000

    updated .gitignore

--- 01/HelloWorld.cpp
+++ 01/HelloWorld.cpp
@@ -10,6 +10,8 @@
 // main entry poit to the program
 // the function tha is executed by default
 int main() {
+
+  
   std::cout << "Hi there!!! This is Stefano!!!" << std::endl;
   std::cout << std::endl << std::endl;
   return(0);
